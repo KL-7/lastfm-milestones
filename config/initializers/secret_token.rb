6e33b944d33671c2e0173657e508cd053776c397
Kirill Lashuk <kl7.666@gmail.com>
2011-05-02 20:38:35 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LastfmMilestones::Application.config.secret_token = '92158f7844f36e1d3b8ab85e9219534169a209af693b9e85343e93bf0b5bf6f6051f5ad076ae88dd654b103f0251c0d697080b566bd4067bfcf865a77631844e'

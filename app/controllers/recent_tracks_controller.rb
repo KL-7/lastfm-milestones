e9a4e0b48f60651df9236653a293d4a384fae1af
Kirill Lashuk <kl7.666@gmail.com>
2011-05-03 19:56:47 +0300

    Handle errors; link to artists and tracks

--- app/controllers/recent_tracks_controller.rb
+++ app/controllers/recent_tracks_controller.rb
@@ -1,8 +1,13 @@
 class RecentTracksController < ApplicationController
 
   def create
-    @user = Scrobbler::User.new :name => params[:username]
-    @tracks = @user.recent_tracks :page => params[:page].to_i
+    begin
+      @user = Scrobbler::User.new :name => params[:username]
+      @tracks = @user.recent_tracks :page => params[:page].to_i
+      @tracks.map(&:artist).map(&:load_info)
+    rescue Scrobbler::ApiError => e
+      @error = e.message
+    end
   end
 
 end
